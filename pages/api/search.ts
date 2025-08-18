5ecd2745693d49f26b697067148255bbc6463df3
Mr. <gomexxxx71@gmail.com>
2025-08-19 00:17:15 +0600

    chore(api): search characters

---
+++ pages/api/search.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export interface SearchResponse {
+  message: string
+  result: {
+    properties: {
+      name: string
+    }
+    uid: string
+  }[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await fetch(
+    process.env.API_URL! + '/people/?name=' + req.body.name
+  )
+  const r = (await response.json()) as SearchResponse
+
+  if (r.message !== 'ok') {
+    throw new Error('failed to fetch data')
+  }
+
+  res.status(200).json({
+    results: r.result.map(({ uid, properties }) => ({
+      name: properties.name,
+      uid,
+    })),
+  })
+}
