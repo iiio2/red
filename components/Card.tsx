7891d383bca5d67eb23b07e7a749d5ed1fcdd17d
Mr. <gomexxxx71@gmail.com>
2025-08-19 19:59:08 +0600

    chore: get all characters in the client

---
+++ components/Card.tsx
@@ -0,0 +1,19 @@
+export interface CharacterList {
+  uid: string
+  name: string
+}
+
+const Card = ({ uid, name }: CharacterList) => {
+  return (
+    <div className='flex gap-4 items-center flex-col sm:flex-row'>
+      <div className='rounded overflow-hidden shadow-lg w-full sm:w-64 h-32'>
+        <div className='px-6 py-4'>
+          <div className='font-bold text-xl mb-2 truncate'>{name}</div>
+          <p className='text-gray-700 text-base'>{uid}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
